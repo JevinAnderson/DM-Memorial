5ed199f85f08cae3ab3acf2c1769baa106f4075c
Jevin <jevanderson@paypal.com>
2019-11-24 03:04:19 -0800

    Config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Darkmists Memorial`,
+    description: `An homage to the Darkmists mud from www.darkmists.org`,
+    author: `Jevin Anderson <jevinanderson@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
