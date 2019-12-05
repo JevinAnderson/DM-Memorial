8921a16e21f02b6b90d9f44529c9646db36a79dd
Jevin <jevanderson@paypal.com>
2019-12-05 09:15:06 -0800

    Added third hall

--- src/pages/valhalla/hall-3.js
+++ src/pages/valhalla/hall-3.js
@@ -16,14 +16,14 @@ const Hall = () => (
         as you walk along the golden floor. The hall is silent, save for your
         footsteps, as you travel its length. Golden doorways can occasionally be
         seen on either side of the marble walls. The nearest door on the eastern
-        wall is inscribed with the word, "Domination". Opposite it on the
-        western wall is a door marked, "Virtue".
+        wall is inscribed with the word, "Light". Opposite it on the western
+        wall is a door marked, "Darkness".
       </p>
       <Exits>
-        <Link to="/valhalla/hall-3">north</Link>
-        <Link to="/valhalla/fortress">east</Link>
-        <Link to="/valhalla/hall">south</Link>
-        <Link to="/valhalla/castle">west</Link>
+        <Link to="/valhalla/hall-4">north</Link>
+        <Link to="/valhalla/dome">east</Link>
+        <Link to="/valhalla/hall-2">south</Link>
+        <Link to="/valhalla/cave">west</Link>
       </Exits>
     </Room>
   </Layout>
