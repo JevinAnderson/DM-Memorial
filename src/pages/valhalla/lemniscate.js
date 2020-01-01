659c3c992c3f12437dc47c029358463ecae319ba
Jevin <jevanderson@paypal.com>
2020-01-01 15:59:26 -0800

    Update lemniscate.js

--- src/pages/valhalla/lemniscate.js
+++ src/pages/valhalla/lemniscate.js
@@ -14,11 +14,12 @@ const Banquet = () => (
       <p>
         Lady Xyza, The One True Spell, floats in the air here. The weave is all
         around you here, visible to your eyes as infinite miniscule threads of
-        energy intertwining with each forming complex patterns. In the center a
-        large lemniscate hovers, occasionally pulsing with white power. Lady
-        Xyza attends to the weave occasionally, out of habit more than anything.
-        Mostly, she looks down upon her children and smiles. They've all come
-        home. Her work complete, she beams with pride upon what she has wrought.
+        energy intertwining with each other forming complex patterns. In the
+        center a large lemniscate hovers, occasionally pulsing with white power.
+        Lady Xyza attends to the weave occasionally, out of habit more than
+        anything. Mostly she looks down upon her children and smiles. They've
+        all come home. Her work complete, she beams with pride upon what she has
+        wrought.
       </p>
       <Exits>
         <Link to="/valhalla/banquet">down</Link>
